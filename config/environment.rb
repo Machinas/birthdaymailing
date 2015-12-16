7952fec4005d3035343ec8636b720c8f232e9619
eduard herman <herman_eduard2000@yahoo.com>
2015-12-16 17:00:05 +0100

    test2

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.gmail.com',
     :port => 587,
@@ -9,5 +12,3 @@ ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
     :openssl_verify_mode => 'none' 
 }
-# Initialize the Rails application.
-Rails.application.initialize!
