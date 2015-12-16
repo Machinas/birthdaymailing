e82cd441c9bf9d74567a30c242d89e1584252db8
eduard herman <herman_eduard2000@yahoo.com>
2015-12-16 12:18:03 +0100

    v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'localhost',
-  user_name:            'life@machinas.com',
-  password:             '4-low-doors',
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "localhost",
+    :authentication => :plain,
+    :user_name => "life@machinas.com",
+    :password => "4-low-doors"
+  }
+end
+
 
 end
