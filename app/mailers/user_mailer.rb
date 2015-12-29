48d8a9080c8055a1d929d82f4809f3798470309f
eduard herman <herman_eduard2000@yahoo.com>
2015-12-29 11:15:08 +0100

    finnised V1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,4 +2,9 @@ class UserMailer < ApplicationMailer
 	def bdat_mail(name,email)
     mail(:to => email, :from => "life@machinas.com", :subject => "Happy Birthday #{name.capitalize}!!!")
 	end
+	def next_mail(name,date)
+	@users_nob = User.all
+	@todaysdate = Date.today.strftime("%m%d").to_s
+    mail(:to => "eduard@machinas.com, achim@machinas.com, leonard@machinas.com, roman@machinas.com", :from => "life@machinas.com", :subject => "No birthday today... Following Birthdays:")
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,48 @@ class User < ActiveRecord::Base
 	validates :email, :presence=>true
 
 	def self.bday_update
-	    @users = User.all
-	    @users.each do |u|
-	       unless u.accepts_email == false 
-	       	UserMailer.bdat_mail(u.name,u.email).deliver
-	       end
+	@todaysdate = Date.today.strftime("%m%d").to_s
+	@users = User.all
+	@allusersmails = []	
+    @allusersdates = []	
+    @allusersnames = []	
+	@users.each do |x|
+		   	@allusersmails << x.email
+		   	@allusersnames << x.name
+		   	@allusersdates << x.birthday.strftime("%m%d").to_s
     end
+	@users.each do |u|
+	   		@sendmail = false
+
+		   if @todaysdate == u.birthday.strftime("%m%d").to_s
+		   	@birthday_person = u.name
+		   	
+		   	@sendmail = true
+		   end	
+
+		   if @sendmail == true
+		 		unless u.accepts_email == false 
+		 			
+			       	UserMailer.bdat_mail(@birthday_person,@allusersmails.join(",").to_s).deliver
+			      
+			    end
+		   end	
+
+	  
+    end
+    @nobirthday = true
+    @users.each do |z|
+    	if @todaysdate == z.birthday.strftime("%m%d").to_s
+		   	@nobirthday = false
+		end	
+    end	
+
+    if @nobirthday == true
+			
+       	UserMailer.next_mail(@allusersnames.join(",").to_s,@allusersdates.join(",").to_s).deliver
+      
+	end	
+
+
   end
 end
