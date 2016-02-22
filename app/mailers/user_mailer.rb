7913e6c1348eded0a0f2c86abd64262e6da7a262
eduard herman <herman_eduard2000@yahoo.com>
2016-02-22 10:29:00 +0100

    VERSION 10.1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,15 @@
 class UserMailer < ApplicationMailer
+
+
+
 	def bdat_mail(name,email)
-    mail(:to => email, :from => "life@machinas.com", :subject => "Happy Birthday #{name.capitalize}!!!")
+    mail(:to => email, :from => "life@machinas.com", :subject => "Happy Birthday #{name.split.map { |x| x.capitalize }.join(" ")}!!!")
 	end
 	def next_mail(name,date)
-	@users_nob = User.all
+	@users_nob = User.order('users.birthday ASC').all
 	@todaysdate = Date.today.strftime("%m%d").to_s
     mail(:to => "eduard@machinas.com, achim@machinas.com, leonard@machinas.com, roman@machinas.com", :from => "life@machinas.com", :subject => "No birthday today... Following Birthdays:")
 	end
 end
+
+#, achim@machinas.com, leonard@machinas.com, roman@machinas.com
